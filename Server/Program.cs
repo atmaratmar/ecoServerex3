f1c20888806c006c0159dc7ebf51260ed3f47609
atma0004 <atma0004@edu.eal.dk>
2017-08-31 12:03:54 +0200

    Add project files.

---
+++ Server/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+public class EchoServer
+{
+    public static void Main()
+    {
+
+
+        TcpListener listener = null;
+        try
+        {
+
+
+            listener = new TcpListener(IPAddress.Parse("127.0.0.1"), 8080);
+            listener.Start();
+            Console.WriteLine("ready...");
+
+
+            while (true)
+            {
+
+                TcpClient client = listener.AcceptTcpClient();
+
+                StreamReader reader = new StreamReader(client.GetStream());
+                StreamWriter writer = new StreamWriter(client.GetStream());
+                String s = String.Empty;
+                String t = DateTime.Now.ToString("HH:mm:ss");
+                string d = DateTime.Now.ToString("yy:MM:dd");
+                s = reader.ReadLine();
+                if (s == "time")
+
+                {
+
+                    writer.WriteLine("From server -> " + t);
+                    writer.Flush();
+                }
+                else if (s == "date")
+                {
+                    writer.WriteLine("From server -> " + d);
+                    writer.Flush();
+                }
+
+                else
+                {
+                    Console.WriteLine("unknown");
+                    writer.Flush();
+                }
+            }
+            //reader.Close();
+            //writer.Close();
+            //client.Close();
+
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+        }
+
+        finally
+        {
+            if (listener != null)
+            {
+                listener.Stop();
+            }
+        }
+    }
+}    // end Main()} // end class definition
---
+++ chapter19client/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+public class EchoClient
+{
+    public static void Main()
+    {
+        try
+        {
+            
+            
+                //TcpClient client = new TcpClient("127.0.0.1", 8080);
+                //StreamReader reader = new StreamReader(client.GetStream());  move it from here to make a loop
+
+                //StreamWriter writer = new StreamWriter(client.GetStream());
+                //String s = String.Empty;
+                while (true)
+                {
+                TcpClient client = new TcpClient("127.0.0.1", 8080);
+                StreamReader reader = new StreamReader(client.GetStream());
+                StreamWriter writer = new StreamWriter(client.GetStream());
+                String s = String.Empty;
+
+                s = Console.ReadLine();
+                    Console.WriteLine();
+                    writer.WriteLine(s);
+                    writer.Flush();
+                    String server_string = reader.ReadLine();
+                    Console.WriteLine(server_string);
+
+                }
+            
+            //reader.Close();
+            //writer.Close();
+            //client.Close();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+        }
+    } // end Main()
+} // end class definition
